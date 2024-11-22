67e933a9cb1a74020c8d93b89573e671413dae2e
CodeWithPawnesh <pawnesh1999@gmail.com>
2024-11-23 00:42:48 +0530

    first commit

---
+++ src/constants.jsx
@@ -0,0 +1,19 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I used it to make games like this one`,
+  degree: `This is my CS degree.`,
+  sofa: `Here I relax or watch TV.`,
+  table:`This is my table where I place my things like Cell Phone or Books`,
+  chair: `This is a chair.`,
+  wall:`Opps ! you hit a wall.`,
+  door:`Its the entrance of my world`,
+  tv: `That's my TV.`,
+  bed: `This where I sleep.`,
+  resume: `This is my desk and it has my resume. `,
+  projects: `Info about my projects.`,
+  library: `There are a lot of programming books on my shelves. `,
+  exit: `If you want to exit, just close the tab.`,
+  alimira:`This is my almira`,
+  plants:`Plants`
+};
